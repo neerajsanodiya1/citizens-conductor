e86fec01d43e1fd3d705c5f6ac663132b8f8360d
neerajsanodiya1 <neeraj.sanodiya@citizensbank.com>
2023-09-07 20:59:43 +0530

    jdk17
    jdk17

--- cassandra-persistence/src/main/java/com/netflix/conductor/cassandra/dao/CassandraEventHandlerDAO.java
+++ cassandra-persistence/src/main/java/com/netflix/conductor/cassandra/dao/CassandraEventHandlerDAO.java
@@ -113,7 +113,7 @@ public class CassandraEventHandlerDAO extends CassandraBaseDAO implements EventH
             ResultSet resultSet =
                     session.execute(selectAllEventHandlersStatement.bind(HANDLERS_KEY));
             List<Row> rows = resultSet.all();
-            if (rows.size() == 0) {
+            if (rows.isEmpty()) {
                 LOGGER.info("No event handlers were found.");
                 return Collections.EMPTY_LIST;
             }
